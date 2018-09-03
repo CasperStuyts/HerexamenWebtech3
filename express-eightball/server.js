77d673307c4481ab9f6d314c8b1cbd10e8ee0375
CasperStuyts <33087689+CasperStuyts@users.noreply.github.com>
2018-09-03 15:24:05 +0200

    express werkend
    8ball werkt en geeft pseudo-random antwoorden gebaseerd op user input

--- express-eightball/server.js
+++ express-eightball/server.js
@@ -10,3 +10,4 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
